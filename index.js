4017d84e05427d43d53d2336c3e5630ecfffb8af
Luigi Cardito <luigi.cardito@wds.co>
2015-03-03 17:00:50 +0000

    more fun

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send(cool());
